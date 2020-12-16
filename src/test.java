ee1dd35f8f124fc452c2814be10bd0413d495ad9
Ernest <ernestzulpukarov@gmail.com>
2020-12-16 19:25:07 +0700

    a вроде правильно считает, e нет

--- src/test.java
+++ src/test.java
@@ -1,20 +1,21 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class test {
 
   public static void main(String[] args) throws Exception {
-    coordinates coordinates = new coordinates(130000, 0.016, 0.00005, 2, 2, 0);
+    coordinates coordinates;
 
     try (BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"))) {
-      for (double i = 0; i <= 2 * Math.PI; i += 2 * Math.PI / 100) {
-        double[] x = coordinates.coordinates(i);
+      for (double j = 0, i = 0, o = 0, w = 0; j <= 2 * Math.PI;
+          j += 2 * Math.PI / 100, i += 2 * Math.PI / 200, o += 2 * Math.PI / 200, w += 2 * Math.PI / 200) {
+        coordinates = new coordinates(1300000, 0.0004, i, o, w, 0);
+
+        double[] x = coordinates.coordinates(j);
+
         writer.write(x[0] + " " + x[1] + " " + x[2] + "\n");
 
-        System.out.println(coordinates.orbitElemCalc(x, i) + "\n");
-
+        coordinates.orbitElem(x);
       }
     }
 
