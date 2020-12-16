189a49fb9985385d469381202bd81d35e7ff7076
Ernest <ernestzulpukarov@gmail.com>
2020-12-16 14:18:14 +0700

    add V

--- src/coordinates.java
+++ src/coordinates.java
@@ -1,6 +1,9 @@
 import static java.lang.Math.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class coordinates {
 
@@ -24,7 +27,8 @@ public class coordinates {
   double[] coordinates(double t) {
 
     //Вычисление входных параметров по соответствующим формулам
-    double n = 1;
+
+    double n = Math.sqrt(1 / Math.pow(a, 3));
     double M = n * t + M0;
     double E = resultKepler.resultKepler(M, e);
     double v = 2 * atan(Math.sqrt((1 + e) / (1 - e)) * Math.tan(E / 2));
@@ -36,46 +40,55 @@ public class coordinates {
     x[0] = r * (cos(u) * cos(O) - sin(u) * sin(O) * cos(i));
     x[1] = r * (cos(u) * sin(O) + sin(u) * cos(O) * cos(i));
     x[2] = r * sin(u) * sin(i);
-      System.out.print("(r=" + r + ", ");
-      System.out.print("u=" + u + ", ");
-      System.out.print("E=" + E + ", ");
-      System.out.print("v=" + v + ", ");
-      System.out.print("M=" + M + ", ");
-      System.out.println();
+    System.out.print("(r=" + r + ", ");
+    System.out.print("u=" + u + ", ");
+    System.out.print("v=" + v + ", ");
+    System.out.print("E=" + E + ", ");
+    System.out.print("M=" + M + ", ");
+    System.out.println();
+
     return x;
   }
 
   //Метод, возвращающий HashMap параметров орбиты по входному массиву координат
-  public HashMap<String, Double> orbitElemCalc(double[] x, double t) {
+  public Map<String, Double> orbitElemCalc(double[] x, double t) {
 
-   HashMap<String, Double> result = new HashMap<>();
+    Map<String, Double> result = new LinkedHashMap<>();
 
-    double r = sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]);
+    double r = sqrt(x[0] * x[0] + x[1] * x[1] + x[2] * x[2]);
     result.put("r", r);
-    double u = asin(x[2]/(sin(i)*r));
-    if(t<2.7)u=PI-u;
-    else if(t>=2.7)
-    {
-        u=2*PI+u;
-    }
-    if(t>=PI){
-        u=u-2*PI;
-    }
-    if(t>5.9){
-        u=PI-u;
-    }
+    double u = PI - asin(x[2] / (sin(i) * r));
 
     result.put("u", u);
     double v = u - w;
     result.put("v", v);
     double E = 2 * Math.atan(Math.tan(v / 2) / Math.sqrt((1 + e) / (1 - e)));
-    if(t>=PI)E=2*PI+E;
 
     result.put("E", E);
     double M = E - e * Math.sin(E);
     result.put("M", M);
-    return result;
 
+    double v2 = 1 * ((2 / (a * (1 - e * e))) - 1 / a);
+    double vr = sqrt(1 / (1 - e * e)) * e * sin(v);
+    double vn = sqrt(1 / (1 - e * e)) * (1 + e * cos(v));
+
+    double dx = x[0] * vr / r + (-sin(u) * cos(O) - cos(u) * sin(O) * cos(i)) * vn;
+    result.put("dx", dx);
+    double dy = x[1] * vr / r + (-sin(u) * sin(O) + cos(u) * cos(O) * cos(i)) * vn;
+    result.put("dy", dy);
+    double dz = x[2] * vr / r + cos(u) * sin(i) * vn;
+    result.put("dz", dz);
+
+    double V = sqrt(dx * dx + dy * dy + dz * dz);
+    result.put("V", V);
+
+    return result;
   }
+
+//  public double[] SpeedCalc(double u, double v, double[] x) {
+//
+//
+//    return null;
+//  }
 
 }
--- src/test.java
+++ src/test.java
@@ -6,15 +6,14 @@ import java.util.Map;
 public class test {
 
   public static void main(String[] args) throws Exception {
-    coordinates coordinates = new coordinates(1000000, 0.016, 0.00005, 2, 2, 0);
+    coordinates coordinates = new coordinates(130000, 0.016, 0.00005, 2, 2, 0);
 
     try (BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"))) {
-      for (double i = 0; i <=  2*Math.PI; i += 2 * Math.PI / 100) {
+      for (double i = 0; i <= 2 * Math.PI; i += 2 * Math.PI / 100) {
         double[] x = coordinates.coordinates(i);
         writer.write(x[0] + " " + x[1] + " " + x[2] + "\n");
 
-          System.out.println(coordinates.orbitElemCalc(x,i)+"\n");
-
+        System.out.println(coordinates.orbitElemCalc(x, i) + "\n");
 
       }
     }
