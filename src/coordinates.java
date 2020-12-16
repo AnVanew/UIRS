d33eba9945c96935bbebf8d8f050ded72bb8937c
Ernest <ernestzulpukarov@gmail.com>
2020-12-16 19:25:07 +0700

    a вроде правильно считает, e нет

--- src/coordinates.java
+++ src/coordinates.java
@@ -1,14 +1,16 @@
-import static java.lang.Math.*;
+import static java.lang.Math.abs;
+import static java.lang.Math.atan;
+import static java.lang.Math.cos;
+import static java.lang.Math.pow;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class coordinates {
 
   //неизменные параметры орбиты
-  private double a, e, i, O, w, M0;
+  private final double a, e, i, O, w, M0;
 
   public coordinates(double a, double e, double i, double o, double w, double m0) {
     this.a = a;
@@ -29,6 +31,7 @@ public class coordinates {
     //Вычисление входных параметров по соответствующим формулам
 
     double n = Math.sqrt(1 / Math.pow(a, 3));
+//    double n = 1;
     double M = n * t + M0;
     double E = resultKepler.resultKepler(M, e);
     double v = 2 * atan(Math.sqrt((1 + e) / (1 - e)) * Math.tan(E / 2));
@@ -41,8 +44,10 @@ public class coordinates {
     x[1] = r * (cos(u) * sin(O) + sin(u) * cos(O) * cos(i));
     x[2] = r * sin(u) * sin(i);
 
-    double vr = sqrt(1 / (1 - e * e)) * e * sin(v);
-    double vn = sqrt(1 / (1 - e * e)) * (1 + e * cos(v));
+    double p = a * (1 - pow(e, 2));
+    double kor = sqrt(1 / p);
+    double vr = kor * e * sin(v);
+    double vn = kor * (1 + e * cos(v));
 
     x[3] = x[0] * vr / r + (-sin(u) * cos(O) - cos(u) * sin(O) * cos(i)) * vn; //dx
     x[4] = x[1] * vr / r + (-sin(u) * sin(O) + cos(u) * cos(O) * cos(i)) * vn; //dy
@@ -73,6 +78,30 @@ public class coordinates {
 //  }
 
   public HashMap orbitElem(double[] x) {
+    double X = x[0];
+    double Y = x[1];
+    double Z = x[2];
+    double dX = x[3];
+    double dY = x[4];
+    double dZ = x[5];
+
+    double V0 = sqrt(dX * dX + dY * dY + dZ * dZ);
+    double r0 = sqrt(X * X + Y * Y + Z * Z);
+//    double Vc = sqrt(398603.0E9 / r0);
+    double Vc = sqrt(2 / r0);
+
+//    double a = 2 * r0 / (2 - pow((V0 / Vc), 2)); // из учебника
+    double a = 1 / (2 / r0 - V0 * V0 / 1); //из википедии формула, из учебника не сходится
+//    System.out.println(a);
+
+//    double e = sqrt(pow((V0 * V0 / Vc / Vc - 1), 2) + r0 / a * pow(V0 / Vc, 2) * pow(
+//        (X * dX + Y * dY + Z * dZ) / (r0 * V0), 2));
+
+    double e0 = abs(pow(V0 / Vc, 2) - 1);
+    double e = pow(e0, 2) + (1 - pow(e0, 2)) * pow((X * dX + Y * dY + Z * dZ) / (r0 * V0), 2);
+
+    System.out.println(e);
+
     return null;
   }
 
--- src/test.java
+++ src/test.java
@@ -1,20 +1,23 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class test {
 
   public static void main(String[] args) throws Exception {
-    coordinates coordinates = new coordinates(130000, 0.016, 0.00005, 2, 2, 0);
+    coordinates coordinates;
 
     try (BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"))) {
-      for (double i = 0; i <= 2 * Math.PI; i += 2 * Math.PI / 100) {
-        double[] x = coordinates.coordinates(i);
-        writer.write(x[0] + " " + x[1] + " " + x[2] + "\n");
+      for (double j = 0, m = 0, o = 0, w = 0; j <= 2 * Math.PI;
+          j += 2 * Math.PI / 100, m += 2 * Math.PI / 200, o += 2 * Math.PI / 100, w +=
+              2 * Math.PI / 200) {
+        coordinates = new coordinates(1300000, 0.35, 0.005, o, w, m);
 
-        System.out.println(coordinates.orbitElemCalc(x, i) + "\n");
+        double[] x = coordinates.coordinates(j);
 
+        String str = String.format("%20f %20f %20f\n", x[0], x[1], x[2]);
+        writer.write(str);
+
+        coordinates.orbitElem(x);
       }
     }
 
